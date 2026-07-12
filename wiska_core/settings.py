1ecf42ba780fbf2f365327311a934c9cdbf1fb78
sumitexists <sumitsingh290707@gmail.com>
2026-07-13 01:09:24 +0530

    setting done

--- wiska_core/settings.py
+++ wiska_core/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = []
+allowed_hosts_str = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1')
+ALLOWED_HOSTS = [host.strip() for host in allowed_hosts_str.split(',')]
 
 
 # Application definition
